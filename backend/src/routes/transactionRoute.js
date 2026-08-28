418a0e52de1133cc3d40c29a75a24c255ffa8c1f
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-08-28 18:33:19 +0500

    fixed some server files

--- backend/src/routes/transactionRoute.js
+++ backend/src/routes/transactionRoute.js
@@ -6,12 +6,11 @@ import { getSummaryByUserId } from "../controllers/transactionsController.js";
 
 const router=express.Router();
 
+router.get("/summary/:userId",getSummaryByUserId)
 router.get("/:userId",getTransactionsByUserId)
 
 router.post("/", postTransaction)
 
 router.delete("/:userId",deleteTransactionByUserId)
-
-router.get("/summary/:userId",getSummaryByUserId) 
 
 export default router
